fd7c9835caf68daf4943168489d90e3605a15b18
bdimpe@gmail <imaginarydevelopment@gmail.com>
2019-02-08 15:13:23 -0500

    serving different pages based on params

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.get('/', (req,res) => res.sendFile(__dirname + '/public/index.html'))
+const indexHandler = (req,res) =>
+{
+    if(req.query.code == null){
+        res.sendFile(__dirname + '/public/index.html')
+    } else {
+        res.send('Hello code! ' + req.query.code)
+    }
+};
+app.get('/', indexHandler)
 app.get('/hello', (req,res) => res.send('Hello World!'))
 app.get('/markers', (req,res) => res.sendFile(__dirname + '/public/markers.html'))
 app.use(express.static('public', ['html','htm']))
