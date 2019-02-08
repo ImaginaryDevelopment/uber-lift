42d930e58987ffed8da010b34181946828563184
bdimpe@gmail <imaginarydevelopment@gmail.com>
2019-02-08 16:28:51 -0500

    format document, work towards functional bearer

--- ubering.js
+++ ubering.js
@@ -2,29 +2,48 @@ const querystring = require('querystring')
 const request = require('request')
 
 const clientId = process.env.clientid || 'SbTAG12Fz26uhgNZ6qAxxBTiqabpLKlz'
-exports.getBearer = (clientSecret,code,host,port,fBearer,fError) =>{
-        const form = {
-            client_id: clientId,
-            client_secret: clientSecret,
-            grant_type: 'authorization_code',
-            code: code,
-            redirect_uri: 'http://' + host + (port != 80 ? ':' + port : '')
+exports.getBearer = (clientSecret, code, host, port, fBearer, fError) => {
+    const form = {
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: 'http://' + host + (port != 80 ? ':' + port : '')
+    }
+    const formData = querystring.stringify(form)
+    // reference: https://stackoverflow.com/questions/17121846/node-js-how-to-send-headers-with-form-data-using-request-module
+    request({
+        headers: { 'Content-Length': formData.length, 'Content-Type': 'application/x-www-form-urlencoded' },
+        uri: 'https://login.uber.com/oauth/v2/token',
+        body: formData,
+        method: 'POST'
+    }, function (err, res2, body) {
+        console.log('yay response?' + typeof body)
+        const jBody = JSON.parse(body);
+        if (jBody.error) {
+            fError()
+        } else if (jBody.access_token) {
+            fBearer(jBody.access_token);
         }
-        const formData = querystring.stringify(form)
-        // reference: https://stackoverflow.com/questions/17121846/node-js-how-to-send-headers-with-form-data-using-request-module
-        request({
-            headers: { 'Content-Length': formData.length, 'Content-Type': 'application/x-www-form-urlencoded' },
-            uri: 'https://login.uber.com/oauth/v2/token',
-            body: formData,
-            method: 'POST'
-        }, function (err, res2, body) {
-            console.log('yay response?' + typeof body)
-            const jBody = JSON.parse(body);
-            if (jBody.error) {
-                fError()
-            } else if(jBody.access_token){
-                fBearer(jBody.access_token);
-            }
-        })
+    })
+
+}
+exports.getHistory = (bearer,fHistory, fError) => {
+    request({
+        headers: { Authorization: 'Bearer ' + bearer },
+        uri: 'https://api.uber.com/v1.2/history',
+        method: 'GET'
+    }, (err, res, body) => {
+        if(err != null){
+            console.error(JSON.stringify(err))
+            return fError(err,res,body)
+        }
+        if(body == null) return fError(err,res,body)
+        // const jBody = JSON.parse(body)
+        // fHistory(jBody)
+        res.send(body)
+
+    }
+    )
 
 }
