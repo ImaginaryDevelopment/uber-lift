55034bfe9de21edbdc4069215b2ad80e3dced70b
bdimpe@gmail <imaginarydevelopment@gmail.com>
2019-02-09 09:25:21 -0500

    typescript is working, as is the table page

--- public/client/table.jsx
+++ public/client/table.jsx
@@ -1,11 +1,32 @@
 "use strict";
+/// <reference path="../node_modules/@types/react/index.d.ts" />
+/// <reference types="react" />
 (function (context) {
-    var HistoryTable = function () {
+    var HistoryTable = function (_a) {
+        var data = _a.data;
         console.log(context);
         if (context.body == null)
             return <div>Hello React!</div>;
         else
-            return <div>Hello body</div>;
+            return <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Start</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                {data.history.map(function (hi) {
+                return <tr>
+                            <td>{new Date(hi.start_time).toLocaleDateString()}</td>
+                            <td>{hi.start_city.display_name}</td>
+                        </tr>;
+            })}
+                </tbody>
+            </table>
+
+        </div>;
     };
-    ReactDOM.render(<HistoryTable />, document.getElementById('body'));
+    ReactDOM.render(<HistoryTable data={context.data}/>, document.getElementById('body'));
 })(findJsParent());
