2c25def5ec3052c6520c2ea1af00331f82dd406f
bdimpe@gmail <imaginarydevelopment@gmail.com>
2019-02-09 13:30:00 -0500

    centralize table html handler logic

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ const port = process.env.PORT || 3000;
 const app = express()
 app.use(cookieParser())
 
-const err = e => { throw ('Error:' + JSON.stringify(e)) }
-const readFile = (relPath, fText, fErr) => fs.readFile(__dirname + relPath, 'utf8', (err, text) => {
-    if (err != null) return fErr(err)
-    fText(text)
+const readFile = (relPath, fText) => fs.readFile(__dirname + relPath, 'utf8', (err, text) => {
+    if (err != null) throw err
+    return fText(text)
 })
 
 const indexHandler = (req, res) => {
     if (req.cookies != null && req.cookies.bearer != null) {
-        return res.redirect('/home')}
+        return res.redirect('/home')
+    }
     // reference: https://stackoverflow.com/questions/6912584/how-to-get-get-query-string-variables-in-express-js-on-node-js
     if (req.query.code == null) {
         // reference: https://stackoverflow.com/questions/26079611/node-js-typeerror-path-must-be-absolute-or-specify-root-to-res-sendfile-failed
-        //https://login.uber.com/oauth/v2/authorize?response_type=code&client_id=SbTAG12Fz26uhgNZ6qAxxBTiqabpLKlz&scope=history+history_lite+profile&redirect_uri=http://localhost:3000
+        // https://login.uber.com/oauth/v2/authorize?response_type=code&client_id=SbTAG12Fz26uhgNZ6qAxxBTiqabpLKlz&scope=history+history_lite+profile&redirect_uri=http://localhost:3000
         readFile('/public/index.html', html => {
             console.log('indexing', req.headers.host)
             res.send(html.replace('@authUrl', ubering.getAuthUrl(req.headers.host, '')))
@@ -37,15 +37,27 @@ const indexHandler = (req, res) => {
                 res.redirect('/home')
             })
     }
-};
+}
+const getTableHtml = (me, history, f) =>
+    readFile('/public/table.html',
+        html => {
+            // console.log('we have a table')
+            const data = JSON.stringify(history)
+            const pro = JSON.stringify(me)
+            const output = html.replace("data = null", 'data = ' + data).replace("me = null", 'me = ' + pro)
+            // console.log('we have html',me,history,html,f)
+            return f(output)
+        }
+    )
+
 const homeHandler = (req, res) => {
     if (req.cookies == null || req.cookies.bearer == null) return res.redirect('/')
-    ubering.getMe(req.cookies.bearer, (me, _isFull) => {
-        ubering.getHistory(req.cookies.bearer, history => {
-            // res.send(util.inspect(me) + '\r\n' + util.inspect(history))
-            readFile('/public/table.html', html => {
-                res.send(html.replace("body = null", history).replace("me = null", me))
-            })
+    const bearer = req.cookies.bearer
+    ubering.getMe(bearer, (me, _isFull) => {
+        ubering.getHistory(bearer, history => {
+            // necessary https://stackoverflow.com/questions/41801723/express-js-cannot-read-property-req-of-undefined
+            const send = x => res.send(x)
+            return getTableHtml(me, history, send)
         })
     })
 }
@@ -54,17 +66,10 @@ app.get('/', indexHandler)
 app.get('/home', homeHandler)
 app.get('/history/sample/raw', (_req, res) => res.sendfile(__dirname + '/public/samplehistory.json'))
 app.get('/history/sample/table', (_, res) => {
-    fs.readFile(__dirname + '/public/samplehistory.json', 'utf8', (err, raw) => {
-        if (err != null) return sendErrorF(res)(err)
-        fs.readFile(__dirname + '/public/table.html', 'utf8', (err, html) => {
-            if (err != null) return sendErrorF(res)(err)
-            const replaced =
-                html
-                    .replace("data = null", 'data = ' + raw)
-            res.send(replaced)
-        }
+    readFile('/public/sampleuser.json', me =>
+        readFile('/public/samplehistory.json', history =>
+            getTableHtml(me, history, res.send)
         )
-    }
     )
 })
 app.get('/markers', (req, res) => res.sendFile(__dirname + '/public/markers.html'))
@@ -72,6 +77,7 @@ app.use(express.static('client'))
 app.use(express.static('public', ['html', 'htm', 'json']))
 // express error-handling: https://expressjs.com/en/guide/error-handling.html
 app.use(function (err, req, res, next) {
+    console.log('in error handler')
     console.error(err.stack)
     // log this to database in prod, not to customer
     res.status(500).send('Error:' + util.inspect({ err, req, res, next }))
